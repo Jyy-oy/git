91200eca8710856780f3d573241aaa0a254512eb
oyjy <528612252@qq.com>
2022-06-21 19:19:40 +0800

    master third commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -11,5 +11,6 @@ public class git_test {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hot-fix test");
+        System.out.println("master update");
     }
 }
